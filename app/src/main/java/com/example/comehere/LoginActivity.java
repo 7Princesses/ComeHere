eb23a21e07b6e7c0361ce8cfccbc137537da499a
cow-coding <kbp0237@gmail.com>
2021-01-18 00:25:34 +0900

    2. 회원가입 - 회원가입 구현 완료 (테스트 미시행)

--- app/src/main/java/com/example/comehere/LoginActivity.java
+++ app/src/main/java/com/example/comehere/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.comehere;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
@@ -14,6 +16,14 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        TextView registerButton = (TextView) findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/comehere/RegisterActivity.java
+++ app/src/main/java/com/example/comehere/RegisterActivity.java
@@ -1,14 +1,50 @@
 package com.example.comehere;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private final int GET_GALLERY_IMAGE = 200;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        EditText phoneNumber = (EditText) findViewById(R.id.phoneText);
+        // 연락처 입력시 자동으로 (-) 입력
+        phoneNumber.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
+
+        // 학생증 사진 관련 코드
+        imageView = (ImageView)findViewById(R.id.studentCardImage);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,"image/*");
+                startActivityForResult(intent, GET_GALLERY_IMAGE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri selectedImageUri = data.getData();
+            imageView.setImageURI(selectedImageUri);
+        }
     }
 }
