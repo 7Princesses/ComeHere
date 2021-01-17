62335a4a3a9982dc35ee34c68d6fcb28eccde2ab
ownit4137 <ownit4137@users.noreply.github.com>
2021-01-17 16:27:00 +0900

    8. 채팅방 - 단일 채팅방 구현

--- app/src/main/java/com/example/comehere/ChatActivity.java
+++ app/src/main/java/com/example/comehere/ChatActivity.java
@@ -1,27 +1,94 @@
 package com.example.comehere;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChatActivity extends AppCompatActivity {
+
+    private RecyclerView cRecyclerView;
+    private RecyclerView.Adapter cAdapter;
+    private RecyclerView.LayoutManager cLayoutManager;
+    private List<ChatData> chatDataList;
+
+    private EditText et_chat_box;
+    private Button bt_chat_send;
+
+    private String dburl = "https://comehere-cd02d-default-rtdb.firebaseio.com/";
+    private FirebaseDatabase database = FirebaseDatabase.getInstance(dburl);
+    private DatabaseReference myRef = database.getReference("message");
+
+
+    // 임시 이름
+    private String name1 = "user1";
+    private String name2 = "user3";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
+        bt_chat_send = findViewById(R.id.bt_chat_send);
+        et_chat_box = findViewById(R.id.et_chat_box);
+
+        bt_chat_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = et_chat_box.getText().toString();
+                if(msg != null) {
+                    ChatData chat = new ChatData();
+                    chat.setName(name2);    //sender name
+                    chat.setMsg(msg);
+                    et_chat_box.setText(null);
+
+                    myRef.push().setValue(chat);
+                }
+            }
+        });
+
+        cRecyclerView = findViewById(R.id.rv_chat_do);
+        cRecyclerView.setHasFixedSize(true);
+        cLayoutManager = new LinearLayoutManager(this);
+        cRecyclerView.setLayoutManager(cLayoutManager);
+
+        chatDataList = new ArrayList<>();
+        cAdapter = new ChatAdapter(chatDataList, ChatActivity.this, name2);     // app user name
+        cRecyclerView.setAdapter(cAdapter);
 
 
-        // Write a message to the database
 
-        String dburl = "https://comehere-cd02d-default-rtdb.firebaseio.com/";
-        FirebaseDatabase database = FirebaseDatabase.getInstance(dburl);
-        DatabaseReference myRef = database.getReference("message");
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                ChatData chat = dataSnapshot.getValue(ChatData.class);
+                ((ChatAdapter)cAdapter).addChat(chat);
+            }
 
-        myRef.setValue("hello world!");
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {}
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {}
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {}
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {}
+        });
     }
 }
---
+++ app/src/main/java/com/example/comehere/ChatAdapter.java
@@ -0,0 +1,78 @@
+package com.example.comehere;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.MyViewHolder> {
+    private List<ChatData> mDataset;
+    private String myName;
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView tv_name;
+        public TextView tv_msg;
+        public View rootView;
+        public MyViewHolder(View v) {
+            super(v);
+            tv_name = v.findViewById(R.id.tv_name);
+            tv_msg = v.findViewById(R.id.tv_msg);
+            rootView = v;
+        }
+    }
+
+
+    public ChatAdapter(List<ChatData> myDataset, Context context, String myName) {
+        mDataset = myDataset;
+        this.myName = myName;
+    }
+
+
+    @Override
+    public ChatAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LinearLayout v = (LinearLayout) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.row_chat, parent, false);
+
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        ChatData chat = mDataset.get(position);
+
+        holder.tv_name.setText(chat.getName());
+        holder.tv_msg.setText(chat.getMsg());
+
+        if(chat.getName().equals(this.myName)) {
+            holder.tv_msg.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
+            holder.tv_name.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
+        }
+        else {
+            holder.tv_msg.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
+            holder.tv_name.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
+        }
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return mDataset == null ? 0 :  mDataset.size();
+    }
+
+    public ChatData getChat(int position) {
+        return mDataset != null ? mDataset.get(position) : null;
+    }
+
+    public void addChat(ChatData chat) {
+        mDataset.add(chat);
+        notifyItemInserted(mDataset.size()-1);
+    }
+}
