52124f8904b4c944e6fe8da6b978883ad522dbcb
cow-coding <kbp0237@gmail.com>
2021-01-20 00:05:52 +0900

    0. 스플래시 - 스플래시 이미지 추가

--- app/src/main/java/com/example/comehere/Splashscreen.java
+++ app/src/main/java/com/example/comehere/Splashscreen.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -11,56 +12,25 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 public class Splashscreen extends Activity {
-    public void onAttachedToWindow() {
-        super.onAttachedToWindow();
-        Window window = getWindow();
-        window.setFormat(PixelFormat.RGBA_8888);
-    }
-    /** Called when the activity is first created. */
-    Thread splashTread;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
-        StartAnimations();
-    }
-    private void StartAnimations() {
-        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
-        anim.reset();
-        LinearLayout l=(LinearLayout) findViewById(R.id.lin_lay);
-        l.clearAnimation();
-        l.startAnimation(anim);
 
-        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
-        anim.reset();
-        ImageView iv = (ImageView) findViewById(R.id.splash);
-        iv.clearAnimation();
-        iv.startAnimation(anim);
-
-        splashTread = new Thread() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                try {
-                    int waited = 0;
-                    // Splash screen pause time
-                    while (waited < 3500) {
-                        sleep(100);
-                        waited += 100;
-                    }
-                    Intent intent = new Intent(Splashscreen.this,
-                            LoginActivity.class);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                    startActivity(intent);
-                    Splashscreen.this.finish();
-                } catch (InterruptedException e) {
-                    // do nothing
-                } finally {
-                    Splashscreen.this.finish();
-                }
-
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
             }
-        };
-        splashTread.start();
+        }, 1000);
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
